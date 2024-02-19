e5c48f6e6ed9071d947b4b1f3910df37c26db8b3
tarek <tarekelshenawy1997@gmail.com>
2024-02-19 21:14:20 +0200

    amazon clone

---
+++ amazon.js
@@ -0,0 +1,32 @@
+let imags = document.querySelectorAll(".image-slide");
+let prev = document.querySelector(".prev-icon");
+let next = document.querySelector(".next-icon");
+
+let x=0;
+function changepage(){
+    for (i=0;i<imags.length;i++){
+        imags[i].style.display="none"
+    }
+    imags[x].style.display="block";
+
+}
+
+prev.addEventListener("click",(e)=>{
+    if(x>0){
+        x--;
+    }else{
+        x = imags.length -1;
+    }
+    changepage()
+})
+next.addEventListener("click",(e)=>{
+    if(x < imags.length -1){
+        x++;
+    }
+     else{
+        x= 0;
+    }
+    changepage()
+})
+changepage()
+
